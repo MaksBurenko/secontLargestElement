68848846afecc9ac994bc30b8736f6f1b33caa99
Maks Burenko <Cherniymaks@yandex.ru>
2023-03-29 15:06:38 +0300

    Success

--- src/main/java/SecondLargestElement.java
+++ src/main/java/SecondLargestElement.java
@@ -7,12 +7,7 @@ public class SecondLargestElement {
             throw new IllegalArgumentException("Array size should be at least 2!!!");
         }
         Arrays.sort(array);
-        int SLE = 0;
-        for(int i= array.length; i>= array.length-1; i++){
-            if(i== array.length-1){
-                SLE = array[i];
-            }
-        }
-        return SLE;
+
+        return array[array.length-2];
     }
 }
